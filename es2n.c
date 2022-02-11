a0668ef541a719da779e5d54a9bb37a34cf462aa
FilippoRizzato <94640711+FilippoRizzato@users.noreply.github.com>
2022-02-11 10:18:44 +0100

    Create es2n.c

---
+++ es2n.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int int main(int argc, const char** argv) {
+    int n;
+
+do{
+    printf("inserisci n\n"):
+    scanf("%d", &n);
+
+
+
+for (int i = 0; i < n; i++)
+    {
+        if(i % 2){
+            i= i +1;
+            printf("\n I numeri precedenti ad n sono:%d\n",i)
+        }
+        
+      }while(n > 0); 
+        
+    }
+    return 0;
+}
+
